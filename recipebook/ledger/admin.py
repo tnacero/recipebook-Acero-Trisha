4b0e12b63758f800738a1221fb9a6a9e8767074a
Nico Acero <trisha.acero@student.ateneo.edu>
2025-02-27 20:40:52 +0800

    Update admin.py
    Creation of RecipeAdmin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Recipe, RecipeIngredient
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInline,]
+
+admin.site.register(Recipe, RecipeAdmin)
