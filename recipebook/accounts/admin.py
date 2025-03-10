709e41a7f7314a8fbbf6d412274ec2468b8bb6c2
Nico Acero <trisha.acero@student.ateneo.edu>
2025-03-10 21:53:54 +0800

    Created Profile model

--- recipebook/accounts/admin.py
+++ recipebook/accounts/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-
+from .models import Profile
 # Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline,]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin )
 
 
