bc390f979c70541271bc8e8b23751050ec62bd37
Nico Acero <trisha.acero@student.ateneo.edu>
2025-03-10 18:59:27 +0800

    Added recipe author's name to recipe_detail page

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -11,6 +11,9 @@ class Profile(models.Model):
     name = models.CharField(max_length=50)
     bio = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
