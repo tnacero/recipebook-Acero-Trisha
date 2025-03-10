44dd3ddb319394986404b8e3e6d33115eee8bc52
Nico Acero <trisha.acero@student.ateneo.edu>
2025-03-10 17:13:18 +0800

    Created login page

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('ledger.urls', namespace = 'ledger')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
