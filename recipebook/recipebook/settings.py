6ca7b2dbe3aecdfd629287ca5fbd8642ea480431
Nico Acero <trisha.acero@student.ateneo.edu>
2025-03-10 18:14:06 +0800

    Added a logout button and changed the redirects for login and logout

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = '/recipes/list'
+LOGOUT_REDIRECT_URL = '/accounts/login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
